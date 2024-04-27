104d2b982af912779aea360e070a492f378ca168
Hans Jin <hans_jin@126.com>
2024-04-27 10:02:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ base_path = './xtuner'
 os.system(f'git clone https://code.openxlab.org.cn/LiyanJin/xtuner.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
-tokenizer = AutoTokenizer.from_pretrained("./xtuner/InterLM2-Chat-7B",trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained("./xtuner/InterLM2-Chat-7B",trust_remote_code=True, torch_dtype=torch.float16).cuda()
+tokenizer = AutoTokenizer.from_pretrained("./xtuner/",trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("./xtuner/",trust_remote_code=True, torch_dtype=torch.float16).cuda()
 
 def chat(message,history):
     for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
